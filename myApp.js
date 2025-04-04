13ea6f4b734d87d09f0e0945fbc4ccb1aebe3355
Tevin Johnson <johnsontevin@live.com>
2025-04-04 03:22:15 +0000

    2

--- myApp.js
+++ myApp.js
@@ -1,11 +1,17 @@
 let express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 let app = express();
 
 
 
 console.log("Hello World");
 
+app.get("/", handle);
 
+function handle(req, res) {
+    res.send("Hello Express");
+}
 
 
 
